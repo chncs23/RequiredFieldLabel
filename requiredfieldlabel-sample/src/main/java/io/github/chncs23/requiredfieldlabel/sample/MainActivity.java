c6f936500ad7b1c1396d3aaa53aa2eb516fa559f
N. Choatravee <ch.n.17699@gmail.com>
2017-03-10 17:11:02 +0700

    ทำให้ซ่อนและแสดง * ได้

--- requiredfieldlabel-sample/src/main/java/io/github/chncs23/requiredfieldlabel/sample/MainActivity.java
+++ requiredfieldlabel-sample/src/main/java/io/github/chncs23/requiredfieldlabel/sample/MainActivity.java
@@ -2,11 +2,15 @@ package io.github.chncs23.requiredfieldlabel.sample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import io.github.chncs23.requiredfieldlabel.RequiredFieldLabel;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RequiredFieldLabel requiredField = (RequiredFieldLabel) findViewById(R.id.req_field);
+        requiredField.hideRequiredField();
+        requiredField.showRequiredField();
     }
 }
--- requiredfieldlabel/src/main/java/io/github/chncs23/requiredfieldlabel/RequiredFieldLabel.java
+++ requiredfieldlabel/src/main/java/io/github/chncs23/requiredfieldlabel/RequiredFieldLabel.java
@@ -3,15 +3,19 @@ package io.github.chncs23.requiredfieldlabel;
 import android.content.Context;
 import android.graphics.Color;
 import android.support.annotation.ColorInt;
+import android.support.v7.widget.AppCompatTextView;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.style.ForegroundColorSpan;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class RequiredFieldLabel extends TextView {
+public class RequiredFieldLabel extends AppCompatTextView {
 
     private static int starColor = Color.RED;
+    private static boolean isHaveRequiredField = true;
 
     public RequiredFieldLabel(Context context) {
         super(context);
@@ -22,13 +26,27 @@ public class RequiredFieldLabel extends TextView {
     }
 
     @Override public void setText(CharSequence text, BufferType type) {
-        SpannableString spannableString = new SpannableString(String.format("%s *", text.toString()));
-        spannableString.setSpan(new ForegroundColorSpan(starColor), spannableString.length() - 1,
-                spannableString.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        super.setText(spannableString, type);
+        if (isHaveRequiredField) {
+            SpannableString spannableString = new SpannableString(String.format("%s *", text.toString()));
+            spannableString.setSpan(new ForegroundColorSpan(starColor), spannableString.length() - 1,
+                    spannableString.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            super.setText(spannableString, type);
+        } else {
+            super.setText(text, type);
+        }
     }
 
     public static void setStarColor(@ColorInt int color) {
         RequiredFieldLabel.starColor = color;
+    }
+
+    public void hideRequiredField() {
+        isHaveRequiredField = false;
+        this.setText(getText().toString().replace(" *", ""));
+    }
+
+    public void showRequiredField() {
+        isHaveRequiredField = true;
+        this.setText(getText().toString());
     }
 }
